d01c7707538877f7dbcabc40516729e270d61600
Spagrrt <chibs3089@gmail.com>
2022-06-12 17:27:50 -0500

    Testing Github Desktop
    I was unsure if I had it set up correctly but I know I do now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import engine.Window;
 
 public class Main {
     public static void main(String[] args){
+        System.out.println("Running Main");
         Window window = Window.get();
         window.run();
     }
